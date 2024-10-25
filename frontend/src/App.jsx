ca29cca62df72d06cce002d0c1506fbfa5da8c5e
Pratheek555 <pratheekravikumar55@gmail.com>
2024-10-26 01:23:04 +0530

    updated background lines feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import {
   Route
 } from "react-router-dom"
 import Signup from './pages/Signup'
-
+import ClubCreation from "./components/ui/ClubCreation"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,7 +20,7 @@ function App() {
         <Route path='/signup' element={<Signup />}></Route>
         {/* <Route path='/signin' element={<Signin />}></Route> */}
         {/* <Route path='/Clubs&Chapters'></Route> */}
-
+        <Route path="/Club-Creation" element={<ClubCreation />}></Route>
       </Routes>
     </BrowserRouter>
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,11 +1,14 @@
 import SignupForm from "../components/ui/SignupForm"
+import { BackgroundLines } from "../components/ui/BackgroundLines"
 
 
 export default function Signup() {
     return (
         <div className="">
+            <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+                <SignupForm />
+            </BackgroundLines>
 
-            <SignupForm />
         </div>
     )
 }
