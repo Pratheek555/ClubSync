bf869077ef57ad1a7ade1f772976ecea8f8b598c
Pratheek555 <pratheekravikumar55@gmail.com>
2024-10-26 02:35:23 +0530

    club creation page

--- frontend/src/components/ui/ClubCreation.jsx
+++ frontend/src/components/ui/ClubCreation.jsx
@@ -7,6 +7,17 @@ import {
     IconBrandGoogle,
     IconBrandOnlyfans,
 } from "@tabler/icons-react";
+import { Autocomplete, AutocompleteSection, AutocompleteItem } from "@nextui-org/autocomplete";
+import { WavyBackground } from "../ui/wavy-background";
+
+const animals = [
+    { label: "Photography", value: "Photography", description: "People Interested in photography" },
+    { label: "Web development", value: "Web development", description: "People interested in learning web development" },
+    { label: "Game Development", value: "Game development", description: "People Interested in Game Development" },
+    { label: "Cloud and DevOps", value: "Cloud and Devops", description: "People Interested in photography" },
+    { label: "BlockChain", value: "BlockChain", description: "People Interested in blockchain" },
+    { label: "Dance", value: "Dance", description: "People Interested in Dance" },
+];
 
 export default function ClubCreation() {
     const handleSubmit = (e) => {
@@ -15,75 +26,66 @@ export default function ClubCreation() {
     };
 
     return (
-        <div className="h-screen flex items-center justify-center">
-            <div className="max-w-md w-full mx-auto p-4 md:p-8 shadow-input bg-white dark:bg-black rounded-none md:rounded-2xl">
-                <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200 text-center">
-                    Amen to the New Journey!
-                </h2>
-                <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center">
-                    Create the Club of your Dreams and Inspire your peers!
-                </p>
+        <WavyBackground className="max-w-4xl mx-auto pb-40">
+            <div className="h-screen flex items-center justify-center">
+                <div className="max-w-md w-full mx-auto p-4 md:p-8 shadow-input bg-white dark:bg-black rounded-none md:rounded-2xl">
+                    <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200 text-center">
+                        Amen to the New Journey!
+                    </h2>
+                    <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300 text-center">
+                        Create the Club of your Dreams and Inspire your peers!
+                    </p>
 
-                <form className="my-8" onSubmit={handleSubmit}>
-                    <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                        <LabelInputContainer>
-                            <Label htmlFor="firstname">Club Name</Label>
-                            <Input id="firstname" placeholder="Open Source Community" type="text" />
+                    <form className="my-8" onSubmit={handleSubmit}>
+                        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+                            <LabelInputContainer>
+                                <Label htmlFor="firstname">Club Name</Label>
+                                <Input id="firstname" placeholder="Open Source Community" type="text" />
+                            </LabelInputContainer>
+
+                        </div>
+
+                        <LabelInputContainer className="mb-4">
+                            <Label htmlFor="email">Description</Label>
+                            <Input id="email" placeholder="Write More about the Club" type="email" />
                         </LabelInputContainer>
 
-                    </div>
+                        <LabelInputContainer className="mb-4">
+                            <Label htmlFor="password">Label</Label>
+                            <Autocomplete
+                                label="Select a Category"
+                                className="max-w-xs"
+                            >
+                                {animals.map((animal) => (
+                                    <AutocompleteItem key={animal.value} value={animal.value}>
+                                        {animal.label}
+                                    </AutocompleteItem>
+                                ))}
+                            </Autocomplete>
 
-                    <LabelInputContainer className="mb-4">
-                        <Label htmlFor="email">Description</Label>
-                        <Input id="email" placeholder="Write More about the Club" type="email" />
-                    </LabelInputContainer>
 
-                    <LabelInputContainer className="mb-4">
-                        <Label htmlFor="password">Labels</Label>
-                        <Input id="password" placeholder="••••••••" type="password" />
-                    </LabelInputContainer>
+                        </LabelInputContainer>
+                        <LabelInputContainer className="mb-4">
+                            <Label htmlFor="email">Club Coordinator</Label>
+                            <Input id="email" placeholder="Write More about the Club" type="email" />
+                        </LabelInputContainer>
 
-                    <button
-                        className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-                        type="submit"
-                    >
-                        Sign up &rarr;
-                        <BottomGradient />
-                    </button>
-
-                    <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-                    <div className="flex flex-col space-y-4">
                         <button
-                            className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                            type="button"
+                            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                            type="submit"
                         >
-                            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                            <span className="text-neutral-700 dark:text-neutral-300 text-sm">GitHub</span>
+                            Let's Start &rarr;
                             <BottomGradient />
                         </button>
 
-                        <button
-                            className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                            type="button"
-                        >
-                            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                            <span className="text-neutral-700 dark:text-neutral-300 text-sm">Google</span>
-                            <BottomGradient />
-                        </button>
+                        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
-                        <button
-                            className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                            type="button"
-                        >
-                            <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                            <span className="text-neutral-700 dark:text-neutral-300 text-sm">OnlyFans</span>
-                            <BottomGradient />
-                        </button>
-                    </div>
-                </form>
+
+                    </form>
+                </div>
             </div>
-        </div>
+        </WavyBackground>
+
     );
 }
 
